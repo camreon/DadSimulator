cf9077d730343bac5f305f468639be7897d51be4
Cameron Guthrie <cguthrie7@gatech.edu>
2016-01-30 22:50:52 -0600

    press ~ ` to lock cursor, esc to unlock

---
+++ Assets/CursorLockScript.cs
@@ -0,0 +1,24 @@
+﻿/* based on http://docs.unity3d.com/ScriptReference/Cursor-lockState.html */
+
+using UnityEngine;
+using System.Collections;
+
+public class CursorLockScript : MonoBehaviour {
+
+	void Update ()
+	{
+		// Release cursor on escape keypress
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		// Confine cursor on tilde/back quote keypress
+		if (Input.GetKeyDown (KeyCode.BackQuote)) {
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+
+			Debug.Log ("Cursor.lockstate = " + Cursor.lockState);
+		}
+	}
+}
